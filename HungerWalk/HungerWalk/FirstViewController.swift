685a9b56efd24ff49b77a0904ada28c2564808ef
Deepesh Mehta <deepeshhmehta@gmail.com>
2018-07-10 15:48:21 -0400

    project created iOS

---
+++ HungerWalk/HungerWalk/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HungerWalk
+//
+//  Created by Deepesh Mehta on 2018-07-10.
+//  Copyright © 2018 DGames. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
