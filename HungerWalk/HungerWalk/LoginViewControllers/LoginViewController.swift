9f76db0ac1f5a08c822c9b9db5dec35402a32d70
Deepesh Mehta <deepeshhmehta@gmail.com>
2018-07-30 18:31:56 -0400

    animating logo on login screen

--- HungerWalk/HungerWalk/LoginViewControllers/LoginViewController.swift
+++ HungerWalk/HungerWalk/LoginViewControllers/LoginViewController.swift
@@ -14,6 +14,8 @@ class LoginViewController: UIViewController {
     
     @IBOutlet var password: UITextField!
     
+    @IBOutlet var logo: UIImageView!
+    
     var keyboardAdjusted = false
     var lastKeyboardOffset: CGFloat = 0.0
 
@@ -24,6 +26,15 @@ class LoginViewController: UIViewController {
         
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+            UIView.animateKeyframes(withDuration: 2.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+                let frame = self.logo.frame
+                let animatedFrame = CGRect(x: frame.minX - frame.width, y: frame.minY - frame.height, width: frame.width * 3, height: frame.height * 3)
+                self.logo.frame = animatedFrame
+                
+            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -76,6 +87,11 @@ class LoginViewController: UIViewController {
     @IBAction func signUpTouched(_ sender: Any) {
        let signUpViewController = storyboard?.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
         self.present(signUpViewController, animated: true)
+    }
+    
+    @IBAction func forgotPasswordTouched(_ sender: Any){
+        let forgotPassword = storyboard?.instantiateViewController(withIdentifier: "ForgotPasswordViewController") as! ForgotPasswordViewController
+        self.present(forgotPassword, animated: true)
     }
 }
 
